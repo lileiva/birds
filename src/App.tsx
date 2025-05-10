1276ca2aeb92e6cce9c40ee17062e02c25673494
Luis Ignacio Leiva Sanchez <lileiva@uc.cl>
2025-05-10 18:11:11 -0400

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { gql } from "@apollo/client";
+import { useEffect } from "react";
+
+import { client } from "./lib/apollo";
+
+const App = () => {
+  useEffect(() => {
+    client
+      .query({
+        query: gql`
+          query Birds {
+            birds {
+              id
+              english_name
+            }
+          }
+        `,
+      })
+      .then((result) => console.log(result.data));
+  }, []);
+
+  return (
+    <div>
+      <h1>Birds</h1>
+    </div>
+  );
+};
+
+export default App;
